4e34ee8d7a7da7ac1fe1dfc3f6ca0f43633ef730
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-04-23 02:14:46 +0300

    get xlsx file and save csv

--- backend/python/parse_xlsx_data.py
+++ backend/python/parse_xlsx_data.py
@@ -1,4 +1,31 @@
 import mysql.connector
+import requests
+import pandas as pd
+
+
+def get_xlsx_to_csv(url, csv_file):
+
+    # replace with the URL of the XLSX file you want to download
+    # url = "https://www.bankofgreece.gr/OpenDataSetFilesALL/STATISTIKI/BoG_Travel_by_region_el_2023-03-31.xlsx"
+    # replace with the filename you want to save the downloaded file as
+    filename = "xlsx/tmp.xlsx"
+
+    response = requests.get(url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+    print("File downloaded successfully!")
+
+    # Load the XLSX file into a Pandas DataFrame
+    df = pd.read_excel(filename)
+
+    # Write the DataFrame to a CSV file
+    # filename_csv = "csv/BoG_Travel_by_region_el_2023-03-31.csv"
+    filename_csv = "csv/" + csv_file
+    df.to_csv(filename_csv, index=False)
+
+    print("File converted successfully!")
+
 
 cnx = mysql.connector.connect(
     host="localhost",
@@ -20,6 +47,7 @@ for row in rows:
     descr = row[4]
     print("id={}, url={}, url_xlsx={}, csv_filename={}, descr={}".format(
         id, url, url_xlsx, csv_filename, descr))
+    get_xlsx_to_csv(url_xlsx, csv_filename)
 
 cursor.close()
 cnx.close()
